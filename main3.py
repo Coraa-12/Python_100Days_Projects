4dbffcb261fb5dbb8cc5dd2afa75971318dfb5dd
Tegar Wijaya Kusuma <tegarwijayakusuma2004@gmail.com>
2024-11-16 20:38:16 +0700

    Messing with if-statement

---
+++ main3.py
@@ -0,0 +1,4 @@
+height = int(input("Enter your height in cm: "))
+
+can_ride = "You can ride the rollercoaster!" if height >= 120 else "Sorry, you have to grow taller before you can ride."
+print(can_ride)
